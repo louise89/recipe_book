fbc836c24da300ac7de56b965558fc3d8e835c08
Louise Whittle <louise.whittle@hotmail.com>
2017-10-05 15:38:32 +0100

    fix everything

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,42 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
-end
+  def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(params.require(:recipe).permit(:title, :servings, :time, :ingredients, :method))
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(params.require(:recipe).permit(:title, :servings, :time, :ingredients, :method))
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+private
+  def recipe_params
+    params.require(:recipe).permit(:title, :servings, :time, :ingredients, :method)
+  end
+
+  end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  validates :title, :ingredients, :method, presence: true,
+                   length: { minimum: 5 }
+  validates :servings, :time, presence: true,
+                    length: { minimum: 1 }
 end
