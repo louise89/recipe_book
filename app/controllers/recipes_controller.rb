3e531a3709a83277a8730d84c9d875b16edadc6a
Louise Whittle <louise.whittle@hotmail.com>
2017-10-05 22:27:22 +0100

    created the first model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(params.require(:recipe).permit(:title, :servings, :time, :ingredients, :method))
+    @recipe = Recipe.new(recipe_params)
 
     if @recipe.save
       redirect_to @recipe
@@ -27,11 +27,18 @@ class RecipesController < ApplicationController
 
   def update
     @recipe = Recipe.find(params[:id])
-    if @recipe.update(params.require(:recipe).permit(:title, :servings, :time, :ingredients, :method))
+    if @recipe.update(recipe_params)
       redirect_to @recipe
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
   end
 
 private
