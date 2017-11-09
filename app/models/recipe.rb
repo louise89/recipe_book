1403ed0a9ae49ca88321d770dbfe91fd763c4c90
Louise Whittle <louise.whittle@hotmail.com>
2017-11-09 11:11:04 +0000

    show multiple ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,18 +7,43 @@ class Recipe < ApplicationRecord
                     length: { minimum: 1 }
 
 
-def recipe_id
-  @recipe_id ||= Recipe.find(id)
-end
+  def recipe_id
+    @recipe_id ||= Recipe.find(id)
+  end
 
-def recipe_ingredients
-  @rec_ing = RecipeIngredient.find_by("recipe_id = ?", recipe_id)
-  @rec_ing.ingredient_id
-end
+  def recipe_ingredients
+    ingredients = []
+    @rec_ing = RecipeIngredient.where("recipe_id = ?", recipe_id).each do |i|
+      ingredients << i.ingredient_id
+    end
+    ingredients
+  end
 
-def ingredient_name
-  @ing_name  = Ingredient.find_by("id = ?", recipe_ingredients)
-  @ing_name.ingredient
-end
+  def ingredient_name
+    ingredient_name = []
+    @ing_name  = Ingredient.find(recipe_ingredients).each do |n|
+      ingredient_name << n.ingredient
+    end
+    ingredient_name
+  end
 
+  def recipe_methods
+    methods = []
+    @rec_meth = RecipeMethod.where("recipe_id = ?", recipe_id).each do |i|
+      methods << i.cooking_method_id
+    end
+    methods
+  end
+
+  def cooking_method_name
+    cooking_method_name = []
+    @cook_meth  = CookingMethod.find(recipe_methods).each do |n|
+      cooking_method_name << n.action
+    end
+    cooking_method_name
+  end
+
+  def category_name=(name)
+     self.category = Ingredient.find_or_create_by(:ingredient => ingredient)
+  end
 end
